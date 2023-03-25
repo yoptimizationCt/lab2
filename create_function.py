7eb2733dc7dabd6eeda33f4d8273cd709e922cf2
Artem <334947@niuitmo.ru>
2023-03-25 17:32:35 +0300

    Явное задание функции

---
+++ create_function.py
@@ -0,0 +1,16 @@
+def create_function(points):
+    # a^2 * \sum x_i^2 + b^2 * n + \sum y_i^2 + 2ab * \sum x_i - 2a*\sum x_i*y_i - 2b \sum y_i
+    n = len(points)
+    summ_x_sqr = 0
+    summ_y_sqr = 0
+    summ_x = 0
+    summ_y = 0
+    summ_x_y = 0
+    for [x, y] in points:
+        summ_x += x
+        summ_y += y
+        summ_x_y += x * y
+        summ_x_sqr += x * x
+        summ_y_sqr += y * y
+    return lambda arg: arg[0] ** 2 * summ_x_sqr + arg[1] ** 2 * n + summ_y_sqr + 2 * summ_x * arg[0] * arg[1] - 2 * arg[
+        0] * summ_x_y - 2 * arg[1] * summ_y
