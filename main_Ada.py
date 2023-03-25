6f7439711a9b92d639187e84e8e9b07da299dfc9
Ultimatereo <mansursult9@gmail.com>
2023-03-25 20:05:36 +0300

    Raw adagrad is made

--- main_Ada.py
+++ main_Ada.py
@@ -5,12 +5,11 @@ plt.rcParams["figure.figsize"] = (10, 10)
 n = 1000
 X = np.random.rand(n) * 10
 Y = 5 * X + np.random.rand(n) * 20
-gamma = 0.5
 fig, ax = plt.subplots()
 ax.scatter(X, Y)
 
 point = np.zeros(2)
-h = 10e-6
+eps = 10e-6
 lr = 0.0009
 v = np.zeros(2)
 
@@ -21,10 +20,12 @@ def gradient(point, j, X):
          2 * (point[0] * X[j] + point[1] - Y[j])])
 
 
+state_sum = 0
 for epoch in range(1, 100):
     j = np.random.randint(0, n)
-    v = gamma * v + (1 - gamma) * gradient(point - lr*gamma*v, j, X)
-    point = point - lr * v
+    gr = gradient(point, j, X)
+    state_sum += gr*gr
+    point = point - lr * gr / (np.sqrt(state_sum) + eps)
     # v = gamma * v_prev - lr * grad
     # point = point
 print(point)
