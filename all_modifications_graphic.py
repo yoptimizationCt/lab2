5a93ee13a288339594a363aad7c4f50603a89570
Ultimatereo <mansursult9@gmail.com>
2023-04-16 21:48:07 +0300

    all modifications are gathered in one place and we can make graphics now!

--- all_modifications_graphic.py
+++ all_modifications_graphic.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from paint_contour import paint_contour
-
+from create_function import create_function_XY
 n = 1000
 X = np.random.rand(n) * 10
-Y = 5 * X + np.random.rand(n) * 20
+Y = 10 * X + np.random.rand(n) * 1
 
 
 def gradient(point, j):
@@ -14,48 +15,83 @@ def gradient(point, j):
 
 
 def SGD(point=np.zeros(2), lr=0.0009, epoch_count=100):
-    for epoch in range(1, epoch_count):
+    points = np.zeros((epoch_count + 1, 2))
+    for epoch in range(1, epoch_count + 1):
         j = np.random.randint(0, n)
         gr = gradient(point, j)
-        point = point - lr * gr
-    return point
+        points[epoch] = points[epoch - 1] - lr * gr
+    return points
 
 
 def SGD_with_momentum(point=np.zeros(2), lr=0.0009, v=np.zeros(2), gamma=0.5, epoch_count=100):
-    for epoch in range(1, epoch_count):
+    points = np.zeros((epoch_count + 1, 2))
+    for epoch in range(1, epoch_count + 1):
         j = np.random.randint(0, n)
         v = gamma * v + (1 - gamma) * gradient(point, j)
-        point = point - lr * v
-    return point
+        points[epoch] = points[epoch - 1] - lr * v
+    return points
 
 
 def Nesterov(point=np.zeros(2), lr=0.0009, v=np.zeros(2), gamma=0.5, epoch_count=100):
-    for epoch in range(1, epoch_count):
+    points = np.zeros((epoch_count + 1, 2))
+    for epoch in range(1, epoch_count + 1):
         j = np.random.randint(0, n)
         v = gamma * v + (1 - gamma) * gradient(point - lr * gamma * v, j)
-        point = point - lr * v
-    return point
+        points[epoch] = points[epoch - 1] - lr * v
+    return points
 
 
-def RMS_prop(point=np.zeros(2), lr=0.0009, epoch_count=100, alpha=0.5, eps=10e-8):
-    s = 0
-    for epoch in range(1, epoch_count):
+def AdaGrad(point=np.zeros(2), lr=0.1, epoch_count=100, eps=10e-8):
+    state_sum = 0
+    points = np.zeros((epoch_count + 1, 2))
+    for epoch in range(1, epoch_count + 1):
         j = np.random.randint(0, n)
-        gr = gradient(point, j, X)
+        gr = gradient(point, j)
+        state_sum += gr * gr
+        points[epoch] = points[epoch - 1] - lr * gr / (np.sqrt(state_sum) + eps)
+    return points
+
+
+def RMS_prop(point=np.zeros(2), lr=0.1, epoch_count=100, alpha=0.5, eps=10e-8):
+    points = np.zeros((epoch_count + 1, 2))
+    s = 0
+    for epoch in range(1, epoch_count + 1):
+        j = np.random.randint(0, n)
+        gr = gradient(point, j)
         s = alpha * s + (1 - alpha) * (gr * gr)
-        point = point - lr * gr / (np.sqrt(s + eps))
-    return point
+        points[epoch] = points[epoch - 1] - lr * gr / (np.sqrt(s + eps))
+    return points
 
 
-def Adam(point=np.zeros(2), lr=0.0009, epoch_count=100, eps=10e-8, beta1=0.9, beta2=0.999):
+def Adam(point=np.zeros(2), lr=0.1, epoch_count=100, eps=10e-8, beta1=0.9, beta2=0.999):
+    points = np.zeros((epoch_count + 1, 2))
     s = 0
     v = 0
-    for epoch in range(1, epoch_count):
+    for epoch in range(1, epoch_count + 1):
         j = np.random.randint(0, n)
         gr = gradient(point, j)
         v = beta1 * v + (1 - beta1) * gr
         s = beta2 * s + (1 - beta2) * (gr * gr)
         vv = v / (1 - beta1 ** (epoch + 1))
         ss = s / (1 - beta2 ** (epoch + 1))
-        point = point - lr * vv / (np.sqrt(ss + eps))
-    return point
+        points[epoch] = points[epoch - 1] - lr * vv / (np.sqrt(ss + eps))
+    return points
+
+
+epoch_count = 1000
+points_SGD = SGD(epoch_count=epoch_count)
+points_SGD_with_momentum = SGD_with_momentum(epoch_count=epoch_count)
+points_Nesterov = Nesterov(epoch_count=epoch_count)
+points_AdaGrad = AdaGrad(epoch_count=epoch_count)
+points_RMS_prop = RMS_prop(epoch_count=epoch_count)
+points_Adam = Adam(epoch_count=epoch_count)
+
+print(points_SGD[epoch_count])
+print(points_SGD_with_momentum[epoch_count])
+print(points_Nesterov[epoch_count])
+print(points_AdaGrad[epoch_count])
+print(points_RMS_prop[epoch_count])
+print(points_Adam[epoch_count])
+
+paint_contour(0, 1000, 0, 1000, 1000, points_SGD, create_function_XY(X, Y))
+plt.savefig("SGD.png")
--- create_function.py
+++ create_function.py
@@ -1,3 +1,23 @@
+def create_function_XY(X, Y):
+    # a^2 * \sum x_i^2 + b^2 * n + \sum y_i^2 + 2ab * \sum x_i - 2a*\sum x_i*y_i - 2b \sum y_i
+    n = len(X)
+    summ_x_sqr = 0
+    summ_y_sqr = 0
+    summ_x = 0
+    summ_y = 0
+    summ_x_y = 0
+    for i in range(n):
+        x = X[i]
+        y = Y[i]
+        summ_x += x
+        summ_y += y
+        summ_x_y += x * y
+        summ_x_sqr += x * x
+        summ_y_sqr += y * y
+    return lambda arg: arg[0] ** 2 * summ_x_sqr + arg[1] ** 2 * n + summ_y_sqr + 2 * summ_x * arg[0] * arg[1] - 2 * arg[
+        0] * summ_x_y - 2 * arg[1] * summ_y
+
+
 def create_function(points):
     # a^2 * \sum x_i^2 + b^2 * n + \sum y_i^2 + 2ab * \sum x_i - 2a*\sum x_i*y_i - 2b \sum y_i
     n = len(points)
